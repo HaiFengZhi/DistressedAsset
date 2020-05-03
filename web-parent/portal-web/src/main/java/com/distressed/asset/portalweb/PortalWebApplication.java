75259334510b8f7042b288897680b759b7749328
李雪阳 <458270356@qq.com>
2020-05-03 17:09:42 +0800

    新增了web模块

--- web-parent/portal-web/src/main/java/com/distressed/asset/portalweb/PortalWebApplication.java
+++ web-parent/portal-web/src/main/java/com/distressed/asset/portalweb/PortalWebApplication.java
@@ -2,8 +2,14 @@ package com.distressed.asset.portalweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+//启用feign客户端，可配置多个
+@EnableFeignClients(basePackages ={"com.distressed.asset"})
+//声明为Controller入口
+@RestController
 public class PortalWebApplication {
 
     public static void main(String[] args) {
